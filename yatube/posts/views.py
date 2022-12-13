76335c70f69a57a61cb0c1f9d29c547913d24137
Сергей Смирнов <sm-serega85@yandex.ru>
2022-12-13 21:46:22 +0300

    Sprint 3 Final 3

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render, get_object_or_404
-# Импортируем модель, чтобы обратиться к ней
 from .models import Post, Group
-MAG = 10
+
+
+NUNBER_POSTS = 10
 
 
 def index(request):
-    posts = Post.objects.order_by('-pub_date')[:MAG]
-    title = "Последние обновления на сайте"
+    posts = Post.objects.order_by('-pub_date')[:NUNBER_POSTS]
     context = {
-        "title": title,
         'posts': posts,
     }
     return render(request, 'posts/index.html', context)
@@ -16,10 +15,8 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = group.posts.order_by('-pub_date')[:MAG]
-    title = "Последние обновления на сайте"
+    posts = group.posts.order_by('-pub_date')[:NUNBER_POSTS]
     context = {
-        "title": title,
         'group': group,
         'posts': posts,
     }
